275a6d8d93168a37d8714610fb1f9d2dc3d8565c
Benjamin Seber <benjamin.seber@focus-shift.de>
2022-11-23 23:19:55 +0100

    WIP user account page

--- src/main/java/de/focusshift/zeiterfassung/usermanagement/UserDto.java
+++ src/main/java/de/focusshift/zeiterfassung/usermanagement/UserDto.java
@@ -1,4 +1,4 @@
 package de.focusshift.zeiterfassung.usermanagement;
 
-record UserDto(String firstName, String lastName, String fullName, String email) {
+record UserDto(Long id, String firstName, String lastName, String fullName, String email) {
 }
--- src/main/java/de/focusshift/zeiterfassung/usermanagement/UserManagementController.java
+++ src/main/java/de/focusshift/zeiterfassung/usermanagement/UserManagementController.java
@@ -4,6 +4,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,7 +33,13 @@ class UserManagementController {
         return "usermanagement/users";
     }
 
+    @GetMapping("/{id}/account")
+    String userAccount(@PathVariable("id") String id, Model model) {
+
+        return "usermanagement/user-account";
+    }
+
     static UserDto userToDto(User user) {
-        return new UserDto(user.givenName(), user.familyName(), user.givenName() + " " + user.familyName(), user.email().value());
+        return new UserDto(user.localId().value(), user.givenName(), user.familyName(), user.givenName() + " " + user.familyName(), user.email().value());
     }
 }
