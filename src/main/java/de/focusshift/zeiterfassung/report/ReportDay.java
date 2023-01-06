da3b2761427d248c6098d1e3aa44cbdd60157869
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-01-06 17:44:18 +0100

    do not sum TimeEntry duration when it is a break

--- src/main/java/de/focusshift/zeiterfassung/report/ReportDay.java
+++ src/main/java/de/focusshift/zeiterfassung/report/ReportDay.java
@@ -6,10 +6,13 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
 
+import static java.util.function.Predicate.not;
+
 record ReportDay(LocalDate date, List<ReportDayEntry> reportDayEntries) {
 
     public WorkDuration workDuration() {
         final Duration duration = reportDayEntries.stream()
+            .filter(not(ReportDayEntry::isBreak))
             .map(ReportDayEntry::workDuration)
             .map(WorkDuration::duration)
             .reduce(Duration.ZERO, Duration::plus);
