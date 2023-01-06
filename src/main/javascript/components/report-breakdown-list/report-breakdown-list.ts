72b918e04328f9036c1d7992d56d442560e424ee
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-01-06 22:48:36 +0100

    show data of selected report day only

---
+++ src/main/javascript/components/report-breakdown-list/report-breakdown-list.ts
@@ -0,0 +1,32 @@
+export class ReportBreakdownList extends HTMLOListElement {
+  #cleanup: () => void;
+
+  connectedCallback() {
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const handleDaySelected = (event) => {
+      console.log("day selected", event);
+      const { date } = event.detail;
+      for (const li of this.querySelectorAll("li")) {
+        if (!date || li.dataset.date === date) {
+          li.classList.remove("hidden");
+        } else {
+          li.classList.add("hidden");
+        }
+      }
+    };
+
+    document.addEventListener("graph-day-selected", handleDaySelected);
+
+    this.#cleanup = () => {
+      document.removeEventListener("graph-day-selected", handleDaySelected);
+    };
+  }
+
+  disconnectedCallback() {
+    this.#cleanup();
+  }
+}
+
+customElements.define("z-report-breakdown-list", ReportBreakdownList, {
+  extends: "ol",
+});
