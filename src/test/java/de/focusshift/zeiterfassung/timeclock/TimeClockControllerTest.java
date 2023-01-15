e5e47568a406369ac90f10267ba7a764b2136876
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-01-15 12:06:50 +0100

    add TimeClockController tests

--- src/test/java/de/focusshift/zeiterfassung/timeclock/TimeClockControllerTest.java
+++ src/test/java/de/focusshift/zeiterfassung/timeclock/TimeClockControllerTest.java
@@ -10,17 +10,24 @@ import org.springframework.security.web.method.annotation.AuthenticationPrincipa
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -29,11 +36,51 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 @WebMvcTest(controllers = { TimeClockController.class })
 class TimeClockControllerTest {
 
+    private static final ZoneId ZONE_EUROPE_BERLIN = ZoneId.of("Europe/Berlin");
+
     @Autowired
     private TimeClockController sut;
 
     @MockBean
     private TimeClockService timeClockService;
+
+    @Test
+    void ensureEditTimeClockGetMappingWithoutRunningTimeClock() throws Exception {
+
+        when(timeClockService.getCurrentTimeClock(new UserId("batman"))).thenReturn(Optional.empty());
+
+        perform(
+            get("/timeclock")
+                .with(oidcLogin().userInfoToken(builder -> builder.subject("batman")))
+        )
+            .andExpect(status().isOk())
+            .andExpect(model().attributeDoesNotExist("timeClockUpdate"))
+            .andExpect(view().name("timeclock/timeclock-edit"));
+    }
+
+    @Test
+    void ensureEditTimeClockGetMappingWithRunningTimeClock() throws Exception {
+
+        final ZonedDateTime startedAt = ZonedDateTime.of(2023, 1, 11, 13, 37, 0, 0, ZONE_EUROPE_BERLIN);
+        final TimeClock timeClock = new TimeClock(1L, new UserId("batman"), startedAt, "awesome comment", Optional.empty());
+        when(timeClockService.getCurrentTimeClock(new UserId("batman"))).thenReturn(Optional.of(timeClock));
+
+        perform(
+            get("/timeclock")
+                .with(oidcLogin().userInfoToken(builder -> builder.subject("batman")))
+        )
+            .andExpect(status().isOk())
+            .andExpect(model().attribute("timeClockUpdate",
+                allOf(
+                    hasProperty("startedAt", is(Instant.from(startedAt))),
+                    hasProperty("zoneId", is(ZONE_EUROPE_BERLIN)),
+                    hasProperty("comment", is("awesome comment")),
+                    hasProperty("date", is("2023-01-11")),
+                    hasProperty("time", is("13:37"))
+                )
+            ))
+            .andExpect(view().name("timeclock/timeclock-edit"));
+    }
 
     @Test
     void ensureEditTimeClock() throws Exception {
@@ -50,7 +97,7 @@ class TimeClockControllerTest {
             .andExpect(redirectedUrl("referer-url"));
 
         final UserId expectedUserId = new UserId("batman");
-        final ZonedDateTime expectedZonedDateTime = ZonedDateTime.of(2023, 1, 11, 13, 37, 0, 0, ZoneId.of("Europe/Berlin"));
+        final ZonedDateTime expectedZonedDateTime = ZonedDateTime.of(2023, 1, 11, 13, 37, 0, 0, ZONE_EUROPE_BERLIN);
         verify(timeClockService).updateTimeClock(expectedUserId, new TimeClockUpdate(expectedUserId, expectedZonedDateTime, "awesome comment"));
     }
 
