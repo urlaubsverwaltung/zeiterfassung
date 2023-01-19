4093e64c909ddf28f2c9dca51260dddb13ebb31f
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-01-19 21:55:11 +0100

    fix eslint issues
    FeedbackHeart could be accessible some day. Then the `div` will be a `button` with sound and/or text for the screen reader.

--- src/main/javascript/bundles/user-common.ts
+++ src/main/javascript/bundles/user-common.ts
@@ -5,6 +5,7 @@ import "../components/time-clock";
 import { initFeedbackHeartView } from "../components/feedback-heart";
 
 const showFeedbackKudo =
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).zeiterfassung?.feedback?.showFeedbackKudo ?? false;
 
 initFeedbackHeartView({
