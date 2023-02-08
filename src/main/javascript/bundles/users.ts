7e9a599eaec0452d9406b139b3030a2501502a8c
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-02-08 19:43:52 +0100

    advance user list with turbo

---
+++ src/main/javascript/bundles/users.ts
@@ -0,0 +1,17 @@
+// import * as Turbo from "@hotwired/turbo";
+
+document.addEventListener("turbo:render", function (event) {
+  const main = document.querySelector("main");
+
+  const currentLink = document.querySelector(
+    "a[aria-current='page']",
+  ) as HTMLAnchorElement;
+
+  if (currentLink && currentLink.matches("[data-turbo-frame='person-frame']")) {
+    main.dataset.selectedUser = currentLink.dataset.user;
+  } else {
+    delete main.dataset.selectedUser;
+  }
+});
+
+export {};
