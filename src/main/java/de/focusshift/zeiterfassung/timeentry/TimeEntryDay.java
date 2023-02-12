943c6df8bece02244865b5ee0a8c628e33bc7265
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-02-12 16:40:23 +0100

    cleanup
    not required anymore. we have `WorkDuration` and `BreakDuration` now

--- src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryDay.java
+++ src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryDay.java
@@ -4,15 +4,12 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
 
-import static java.util.function.Predicate.not;
-
 record TimeEntryDay(LocalDate date, List<TimeEntry> timeEntries) {
 
     public WorkDuration workDuration() {
 
         final Duration duration = timeEntries
             .stream()
-            .filter(not(TimeEntry::isBreak))
             .map(TimeEntry::workDuration)
             .map(WorkDuration::value)
             .reduce(Duration.ZERO, Duration::plus);
