urlaubsverwaltung/zeiterfassung

commit 97d9e77fb7fa103f5ba599a4e43a9d9547806f92

ease handling of creating/updating a time entry
updating a time entry now renders the correct updated fields on frontend 🥳
previously `duration` has not been updated.

src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.Clock;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Year;
@@ -120,14 +121,74 @@ class TimeEntryServiceImpl implements TimeEntryService {
     }
 
     @Override
-    public TimeEntry saveTimeEntry(TimeEntry timeEntry) {
-        final TimeEntryEntity savedEntity = timeEntryRepository.save(toEntity(timeEntry, Instant.now(clock)));
-        return toTimeEntry(savedEntity);
+    public TimeEntry createTimeEntry(UserId userId, String comment, ZonedDateTime start, ZonedDateTime end, boolean isBreak) {
+
+        final TimeEntryEntity entity = new TimeEntryEntity();
+        entity.setOwner(userId.value());
+        entity.setComment(comment.strip());
+        entity.setStart(start.toInstant());
+        entity.setStartZoneId(start.getZone().getId());
+        entity.setEnd(end.toInstant());
+        entity.setEndZoneId(end.getZone().getId());
+        entity.setBreak(isBreak);
+
+        return save(entity);
+    }
+
+    @Override
+    public TimeEntry updateTimeEntry(TimeEntryId id, String comment, ZonedDateTime start, ZonedDateTime end,
+                                     Duration duration, boolean isBreak) throws TimeEntryUpdateException {
+
+        final TimeEntryEntity entity = timeEntryRepository.findById(id.value())
+            .orElseThrow(() -> new IllegalStateException("could not find existing timeEntry id=%s".formatted(id)));
+
+        final TimeEntry existingTimeEntry = toTimeEntry(entity);
+        final boolean startChanged = notEquals(existingTimeEntry.start(), start);
+        final boolean endChanged = notEquals(existingTimeEntry.end(), end);
+        final boolean durationChanged = !existingTimeEntry.workDuration().duration().equals(duration);
+
+        if (startChanged && endChanged && durationChanged) {
+            throw new TimeEntryUpdateException("cannot update time-entry when start, end and duration should be changed. pick two of them.");
+        }
+
+        if (startChanged) {
+            entity.setStart(start.toInstant());
+            entity.setStartZoneId(start.getZone().getId());
+            if (durationChanged) {
+                final ZonedDateTime newEnd = start.plusMinutes(duration.toMinutes());
+                entity.setEnd(newEnd.toInstant());
+                entity.setEndZoneId(newEnd.getZone().getId());
+            }
+        }
+
+        if (endChanged) {
+            entity.setEnd(end.toInstant());
+            entity.setEndZoneId(end.getZone().getId());
+            if (durationChanged) {
+                final ZonedDateTime newStart = end.minusMinutes(duration.toMinutes());
+                entity.setStart(newStart.toInstant());
+                entity.setStartZoneId(newStart.getZone().getId());
+            }
+        }
+
+        entity.setComment(comment.strip());
+        entity.setBreak(isBreak);
+
+        return save(entity);
+    }
+
+    private boolean notEquals(ZonedDateTime one, ZonedDateTime two) {
+        return !one.toInstant().atZone(ZoneOffset.UTC).equals(two.toInstant().atZone(ZoneOffset.UTC));
     }
 
     @Override
     public void deleteTimeEntry(long timeEntryId) {
         timeEntryRepository.deleteById(timeEntryId);
+    }
+
+    private TimeEntry save(TimeEntryEntity entity) {
+        entity.setUpdatedAt(Instant.now(clock));
+        return toTimeEntry(timeEntryRepository.save(entity));
     }
 
     private static TimeEntry toTimeEntry(TimeEntryEntity entity) {
@@ -145,16 +206,7 @@ class TimeEntryServiceImpl implements TimeEntryService {
 
         final UserId userId = new UserId(entity.getOwner());
 
-        return new TimeEntry(entity.getId(), userId, entity.getComment(), startDateTime, endDateTime, entity.isBreak());
-    }
-
-    private TimeEntryEntity toEntity(TimeEntry timeEntry, Instant updatedAt) {
-        final Instant start = timeEntry.start().toInstant();
-        final ZoneId startZoneId = timeEntry.start().getZone();
-        final Instant end = timeEntry.end().toInstant();
-        final ZoneId endZoneId = timeEntry.end().getZone();
-
-        return new TimeEntryEntity(timeEntry.id(), timeEntry.userId().value(), timeEntry.comment(), start, startZoneId, end, endZoneId, updatedAt, timeEntry.isBreak());
+        return new TimeEntry(new TimeEntryId(entity.getId()), userId, entity.getComment(), startDateTime, endDateTime, entity.isBreak());
     }
 
     private static Instant toInstant(LocalDate localDate) {

src/main/java/de/focusshift/zeiterfassung/development/DemoDataCreationService.java
@@ -2,7 +2,6 @@ package de.focusshift.zeiterfassung.development;
 
 import de.focusshift.zeiterfassung.tenancy.user.TenantUser;
 import de.focusshift.zeiterfassung.tenancy.user.TenantUserCreatedEvent;
-import de.focusshift.zeiterfassung.timeentry.TimeEntry;
 import de.focusshift.zeiterfassung.timeentry.TimeEntryService;
 import de.focusshift.zeiterfassung.user.UserId;
 import org.slf4j.Logger;
@@ -70,10 +69,13 @@ class DemoDataCreationService {
             LocalTime endTime = randomTimeBetween(startTime, END_OF_WORK_DAY);
 
             while (endTime.isBefore(END_OF_WORK_DAY)) {
-                timeEntryService.saveTimeEntry(new TimeEntry(null, new UserId(user.id()),
-                    getRandomComment(),
-                    ZonedDateTime.of(LocalDateTime.of(actualDate, startTime), ZoneId.of("Europe/Berlin")),
-                    ZonedDateTime.of(LocalDateTime.of(actualDate, endTime), ZoneId.of("Europe/Berlin")), false));
+
+                final UserId userId = new UserId(user.id());
+                final ZoneId zoneId = ZoneId.of("Europe/Berlin");
+                final ZonedDateTime start = ZonedDateTime.of(LocalDateTime.of(actualDate, startTime), zoneId);
+                final ZonedDateTime end = ZonedDateTime.of(LocalDateTime.of(actualDate, endTime), zoneId);
+
+                timeEntryService.createTimeEntry(userId, getRandomComment(), start, end, false);
 
                 startTime = endTime;
                 endTime = randomTimeBetween(startTime, END_OF_WORK_DAY).plusMinutes(30);

src/main/java/de/focusshift/zeiterfassung/timeclock/TimeClockService.java
@@ -50,8 +50,14 @@ class TimeClockService {
             .map(entity -> timeClockEntityWithStoppedAt(entity, ZonedDateTime.now(userSettingsProvider.zoneId())))
             .map(timeClockRepository::save)
             .map(TimeClockService::toTimeClock)
-            .map(TimeClockService::timeClockToTimeEntry)
-            .ifPresent(timeEntryService::saveTimeEntry);
+            .ifPresent(timeClock -> {
+
+                final ZonedDateTime start = timeClock.startedAt();
+                final ZonedDateTime end = timeClock.stoppedAt()
+                    .orElseThrow(() -> new IllegalStateException("expected stoppedAt to contain a value."));
+
+                timeEntryService.createTimeEntry(userId, timeClock.comment(), start, end, timeClock.isBreak());
+            });
     }
 
     private static TimeClockEntity toEntity(TimeClock timeClock) {

src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntry.java
@@ -6,7 +6,7 @@ import java.time.Duration;
 import java.time.ZonedDateTime;
 
 public record TimeEntry(
-    Long id,
+    TimeEntryId id,
     UserId userId,
     String comment,
     ZonedDateTime start,

src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryController.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -40,6 +39,7 @@ import static java.time.Month.DECEMBER;
 import static java.time.temporal.IsoFields.WEEK_OF_WEEK_BASED_YEAR;
 import static java.time.temporal.TemporalAdjusters.previousOrSame;
 import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.util.StringUtils.hasText;
 
 @Controller
 @PreAuthorize("hasRole('ZEITERFASSUNG_USER')")
@@ -75,7 +75,7 @@ class TimeEntryController implements HasTimeClock, HasLaunchpad {
                                                   @RequestHeader(name = "Turbo-Frame", required = false) String turboFrame,
                                                   Model model) {
 
-        if (StringUtils.hasText(turboFrame)) {
+        if (hasText(turboFrame)) {
             prepareTimeEntriesView(year, weekOfYear, model, principal);
             model.addAttribute("turboStreamsEnabled", true);
             return "timeentries/index::#frame-time-entry-weeks";
@@ -146,7 +146,7 @@ class TimeEntryController implements HasTimeClock, HasLaunchpad {
 
         final UserId userId = new UserId(principal.getUserInfo().getSubject());
 
-        if (StringUtils.hasText(turboFrame)) {
+        if (hasText(turboFrame)) {
             final TimeEntryWeekPage entryWeekPage = timeEntryService.getEntryWeekPage(userId, year, weekOfYear);
 
             final TimeEntryDay timeEntryDay = entryWeekPage.timeEntryWeek().days()
@@ -158,7 +158,12 @@ class TimeEntryController implements HasTimeClock, HasLaunchpad {
             final String weekHoursWorked = durationToTimeString(entryWeekPage.timeEntryWeek().workDuration().duration());
             final String dayHoursWorked = durationToTimeString(timeEntryDay.workDuration().duration());
 
-            model.addAttribute("turboEditedTimeEntry", timeEntryDTO);
+            final TimeEntry editedTimeEntry = timeEntryDay.timeEntries().stream()
+                .filter(entry -> entry.id().value().equals(timeEntryDTO.getId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("could not find edited timeEntry=%s".formatted(timeEntryDTO.getId())));
+
+            model.addAttribute("turboEditedTimeEntry", toTimeEntryDto(editedTimeEntry));
             model.addAttribute("calendarWeek", entryWeekPage.timeEntryWeek().week());
             model.addAttribute("workedHoursSumWeek", weekHoursWorked);
             model.addAttribute("workedHoursSumDay", dayHoursWorked);
@@ -172,10 +177,26 @@ class TimeEntryController implements HasTimeClock, HasLaunchpad {
         }
     }
 
-    private String saveOrUpdate(TimeEntryDTO timeEntryDTO, BindingResult bindingResult, Model model, OidcUser principal, HttpServletRequest request) {
+    @PostMapping(value = "/timeentries/{id}", params = "delete")
+    public String delete(@PathVariable("id") Long id, Model model, @AuthenticationPrincipal OidcUser principal) {
+
+        timeEntryService.deleteTimeEntry(id);
+
+        return "redirect:/timeentries";
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/timeentries/{id}", params = "delete", headers = {"X-Requested-With=ajax"})
+    public void deleteJs(@PathVariable("id") Long id, @AuthenticationPrincipal OidcUser principal) {
+
+        timeEntryService.deleteTimeEntry(id);
+    }
+
+    private String saveOrUpdate(TimeEntryDTO dto, BindingResult bindingResult, Model model, OidcUser principal, HttpServletRequest request) {
 
         if (bindingResult.hasErrors()) {
-            model.addAttribute("timeEntryErrorId", timeEntryDTO);
+            model.addAttribute("timeEntryErrorId", dto);
+
             final boolean hasErrorStart = bindingResult.hasFieldErrors("start");
             final boolean hasErrorEnd = bindingResult.hasFieldErrors("end");
             final boolean hasErrorDuration = bindingResult.hasFieldErrors("duration");
@@ -192,27 +213,64 @@ class TimeEntryController implements HasTimeClock, HasLaunchpad {
         }
 
         final UserId userId = new UserId(principal.getUserInfo().getSubject());
-        final TimeEntry timeEntry = toTimeEntry(timeEntryDTO, userId);
+        final ZoneId zoneId = userSettingsProvider.zoneId();
 
-        timeEntryService.saveTimeEntry(timeEntry);
+        if (dto.getId() == null) {
+            createTimeEntry(dto, userId, zoneId);
+        } else {
+            updateTimeEntry(dto, zoneId);
+        }
 
         final String referer = request.getHeader("referer");
         return "redirect:" + referer;
     }
 
-    @PostMapping(value = "/timeentries/{id}", params = "delete")
-    public String delete(@PathVariable("id") Long id, Model model, @AuthenticationPrincipal OidcUser principal) {
+    private void createTimeEntry(TimeEntryDTO dto, UserId userId, ZoneId zoneId) {
 
-        timeEntryService.deleteTimeEntry(id);
+        final ZonedDateTime start;
+        final ZonedDateTime end;
 
-        return "redirect:/timeentries";
+        final Duration duration = toDuration(dto.getDuration());
+
+        if (duration.equals(Duration.ZERO)) {
+            // start and end should be given
+            start = ZonedDateTime.of(LocalDateTime.of(dto.getDate(), dto.getStart()), zoneId);
+            end = getEndDate(dto, zoneId);
+        } else if (dto.getStart() == null) {
+            // end and duration should be given
+            end = ZonedDateTime.of(LocalDateTime.of(dto.getDate(), dto.getEnd()), zoneId);
+            start = end.minusMinutes(duration.toMinutes());
+        } else {
+            // start and duration should be given
+            start = ZonedDateTime.of(LocalDateTime.of(dto.getDate(), dto.getStart()), zoneId);
+            end = start.plusMinutes(duration.toMinutes());
+        }
+
+        timeEntryService.createTimeEntry(userId, dto.getComment(), start, end, dto.isBreak());
     }
 
-    @ResponseBody
-    @PostMapping(value = "/timeentries/{id}", params = "delete", headers = {"X-Requested-With=ajax"})
-    public void deleteJs(@PathVariable("id") Long id, @AuthenticationPrincipal OidcUser principal) {
+    private void updateTimeEntry(TimeEntryDTO dto, ZoneId zoneId) {
 
-        timeEntryService.deleteTimeEntry(id);
+        final Duration duration = toDuration(dto.getDuration());
+        final ZonedDateTime start = ZonedDateTime.of(LocalDateTime.of(dto.getDate(), dto.getStart()), zoneId);
+        final ZonedDateTime end = getEndDate(dto, zoneId);
+
+        try {
+            timeEntryService.updateTimeEntry(new TimeEntryId(dto.getId()), dto.getComment(), start, end, duration, dto.isBreak());
+        } catch (TimeEntryUpdateException e) {
+            LOG.info("could not update time-entry", e);
+            // TODO provide user feedback somehow? or is an error page enough? should not happen often this case?
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ZonedDateTime getEndDate(TimeEntryDTO dto, ZoneId zoneId) {
+        if (dto.getEnd().isBefore(dto.getStart())) {
+            // end is on next day
+            return ZonedDateTime.of(LocalDateTime.of(dto.getDate().plusDays(1), dto.getEnd()), zoneId);
+        } else {
+            return ZonedDateTime.of(LocalDateTime.of(dto.getDate(), dto.getEnd()), zoneId);
+        }
     }
 
     private void addTimeEntryToModel(Model model, TimeEntryDTO timeEntryDTO) {
@@ -290,7 +348,7 @@ class TimeEntryController implements HasTimeClock, HasLaunchpad {
         final String durationString = toTimeEntryDTODurationString(duration);
 
         return TimeEntryDTO.builder()
-            .id(timeEntry.id())
+            .id(timeEntry.id().value())
             .date(date)
             .start(startTime)
             .end(endTime)
@@ -300,61 +358,8 @@ class TimeEntryController implements HasTimeClock, HasLaunchpad {
             .build();
     }
 
-    private TimeEntry toTimeEntry(TimeEntryDTO timeEntryDTO, UserId userId) {
-
-        LocalDate date = timeEntryDTO.getDate();
-        LocalTime startTime = timeEntryDTO.getStart();
-        LocalTime endTime = timeEntryDTO.getEnd();
-        Duration duration = toDuration(timeEntryDTO.getDuration());
-
-        final ZoneId zoneId = userSettingsProvider.zoneId();
-
-        if (startTime != null && endTime == null && duration != null) {
-            endTime = startTime.plusMinutes(duration.toMinutes());
-        } else if (startTime == null && duration != null) {
-            startTime = endTime.minusMinutes(duration.toMinutes());
-        } else if (startTime != null && endTime != null && duration == null) {
-            duration = Duration.between(startTime, endTime);
-        }
-
-        ZonedDateTime start = null;
-        ZonedDateTime end = null;
-
-        if (startTime != null && endTime != null) {
-            start = ZonedDateTime.of(LocalDateTime.of(date, startTime), zoneId);
-            if (endTime.isBefore(startTime)) {
-                end = ZonedDateTime.of(LocalDateTime.of(date.plusDays(1), endTime), zoneId);
-            } else {
-                end = ZonedDateTime.of(LocalDateTime.of(date, endTime), zoneId);
-            }
-        } else if (startTime == null && duration != null) {
-            start = ZonedDateTime.of(LocalDateTime.of(date, endTime), zoneId).minusSeconds(duration.toSeconds());
-            if (endTime.isBefore(startTime)) {
-                end = ZonedDateTime.of(LocalDateTime.of(date.plusDays(1), endTime), zoneId);
-            } else {
-                end = ZonedDateTime.of(LocalDateTime.of(date, endTime), zoneId);
-            }
-        } else if (startTime != null && duration != null) {
-            start = ZonedDateTime.of(LocalDateTime.of(date, startTime), zoneId);
-            if (endTime.isBefore(startTime)) {
-                end = ZonedDateTime.of(LocalDateTime.of(date.plusDays(1), startTime), zoneId).plusSeconds(duration.toSeconds());
-            } else {
-                end = ZonedDateTime.of(LocalDateTime.of(date, startTime), zoneId).plusSeconds(duration.toSeconds());
-            }
-        }
-
-        if (start == null) {
-            LOG.info("start was `null` while converting TimeEntryDTO. seems fishy <°))))><");
-        }
-        if (end == null) {
-            LOG.info("end was `null` while converting TimeEntryDTO. seems fishy <°))))><");
-        }
-
-        return new TimeEntry(timeEntryDTO.getId(), userId, timeEntryDTO.getComment(), start, end, timeEntryDTO.isBreak());
-    }
-
     private static Duration toDuration(String timeEntryDTODurationString) {
-        if (StringUtils.hasText(timeEntryDTODurationString)) {
+        if (hasText(timeEntryDTODurationString)) {
             final String[] split = timeEntryDTODurationString.split(":");
             return Duration.ofHours(Integer.parseInt(split[0])).plusMinutes(Integer.parseInt(split[1]));
         }

src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryEntity.java
@@ -1,7 +1,6 @@
 package de.focusshift.zeiterfassung.timeentry;
 
 import de.focusshift.zeiterfassung.tenancy.tenant.AbstractTenantAwareEntity;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +10,7 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
 import java.time.Instant;
 import java.time.ZoneId;
 import java.util.Objects;
@@ -82,36 +82,72 @@ public class TimeEntryEntity extends AbstractTenantAwareEntity {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getOwner() {
         return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 
     public String getComment() {
         return comment;
     }
 
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
     public Instant getStart() {
         return start;
+    }
+
+    public void setStart(Instant start) {
+        this.start = start;
     }
 
     public String getStartZoneId() {
         return startZoneId;
     }
 
+    public void setStartZoneId(String startZoneId) {
+        this.startZoneId = startZoneId;
+    }
+
     public Instant getEnd() {
         return end;
+    }
+
+    public void setEnd(Instant end) {
+        this.end = end;
     }
 
     public String getEndZoneId() {
         return endZoneId;
     }
 
+    public void setEndZoneId(String endZoneId) {
+        this.endZoneId = endZoneId;
+    }
+
     public Instant getUpdatedAt() {
         return updatedAt;
     }
 
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
     public boolean isBreak() {
         return isBreak;
+    }
+
+    public void setBreak(boolean aBreak) {
+        isBreak = aBreak;
     }
 
     @Override

src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryId.java
@@ -1,0 +1,4 @@
+package de.focusshift.zeiterfassung.timeentry;
+
+public record TimeEntryId(Long value) {
+}

commit 79313e234ae7d7b02ba3e2c5f7e2607206bb9739

fix update time entry which is a break
introduced with commit 97d9e77fb7fa103f5ba599a4e43a9d9547806f92

src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryServiceImpl.java
@@ -145,10 +145,10 @@ class TimeEntryServiceImpl implements TimeEntryService {
         final TimeEntry existingTimeEntry = toTimeEntry(entity);
         final boolean startChanged = notEquals(existingTimeEntry.start(), start);
         final boolean endChanged = notEquals(existingTimeEntry.end(), end);
-        final boolean durationChanged = !existingTimeEntry.workDuration().value().equals(duration);
+        final boolean durationChanged = !existingTimeEntry.duration().value().equals(duration);
 
         if (startChanged && endChanged && durationChanged) {
-            throw new TimeEntryUpdateException("cannot update time-entry when start, end and value should be changed. pick two of them.");
+            throw new TimeEntryUpdateException("cannot update time-entry when start, end and duration should be changed. pick two of them.");
         }
 
         if (startChanged) {

src/test/java/de/focusshift/zeiterfassung/timeentry/TimeEntryServiceImplTest.java
@@ -9,6 +9,8 @@ import de.focusshift.zeiterfassung.usermanagement.UserManagementService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -135,8 +137,9 @@ class TimeEntryServiceImplTest {
         );
     }
 
-    @Test
-    void ensureUpdateTimeEntryStart() throws Exception {
+    @ParameterizedTest
+    @ValueSource(booleans = {true,false})
+    void ensureUpdateTimeEntryStart(boolean isBreak) throws Exception {
 
         final Clock fixedClock = Clock.fixed(Instant.now(), UTC);
         sut = new TimeEntryServiceImpl(timeEntryRepository, userManagementService, userDateService, fixedClock);
@@ -155,7 +158,7 @@ class TimeEntryServiceImplTest {
             entryEnd.toInstant(UTC),
             ZONE_ID_UTC,
             Instant.now(),
-            false);
+            isBreak);
 
         when(timeEntryRepository.findById(42L)).thenReturn(Optional.of(existingEntity));
         when(timeEntryRepository.save(any(TimeEntryEntity.class))).thenAnswer(returnsFirstArg());
@@ -190,8 +193,9 @@ class TimeEntryServiceImplTest {
         assertThat(actualPersisted.isBreak()).isFalse();
     }
 
-    @Test
-    void ensureUpdateTimeEntryStartAndEnd() throws Exception {
+    @ParameterizedTest
+    @ValueSource(booleans = {true,false})
+    void ensureUpdateTimeEntryStartAndEnd(boolean isBreak) throws Exception {
 
         final Clock fixedClock = Clock.fixed(Instant.now(), UTC);
         sut = new TimeEntryServiceImpl(timeEntryRepository, userManagementService, userDateService, fixedClock);
@@ -210,7 +214,7 @@ class TimeEntryServiceImplTest {
             entryEnd.toInstant(UTC),
             ZONE_ID_UTC,
             Instant.now(),
-            false);
+            isBreak);
 
         when(timeEntryRepository.findById(42L)).thenReturn(Optional.of(existingEntity));
         when(timeEntryRepository.save(any(TimeEntryEntity.class))).thenAnswer(returnsFirstArg());
@@ -245,8 +249,9 @@ class TimeEntryServiceImplTest {
         assertThat(actualPersisted.isBreak()).isFalse();
     }
 
-    @Test
-    void ensureUpdateTimeEntryStartAndDuration() throws Exception {
+    @ParameterizedTest
+    @ValueSource(booleans = {true,false})
+    void ensureUpdateTimeEntryStartAndDuration(boolean isBreak) throws Exception {
 
         final Clock fixedClock = Clock.fixed(Instant.now(), UTC);
         sut = new TimeEntryServiceImpl(timeEntryRepository, userManagementService, userDateService, fixedClock);
@@ -265,7 +270,7 @@ class TimeEntryServiceImplTest {
             entryEnd.toInstant(UTC),
             ZONE_ID_UTC,
             Instant.now(),
-            false);
+            isBreak);
 
         when(timeEntryRepository.findById(42L)).thenReturn(Optional.of(existingEntity));
         when(timeEntryRepository.save(any(TimeEntryEntity.class))).thenAnswer(returnsFirstArg());
@@ -300,8 +305,9 @@ class TimeEntryServiceImplTest {
         assertThat(actualPersisted.isBreak()).isFalse();
     }
 
-    @Test
-    void ensureUpdateTimeEntryEnd() throws Exception {
+    @ParameterizedTest
+    @ValueSource(booleans = {true,false})
+    void ensureUpdateTimeEntryEnd(boolean isBreak) throws Exception {
 
         final Clock fixedClock = Clock.fixed(Instant.now(), UTC);
         sut = new TimeEntryServiceImpl(timeEntryRepository, userManagementService, userDateService, fixedClock);
@@ -320,7 +326,7 @@ class TimeEntryServiceImplTest {
             entryEnd.toInstant(UTC),
             ZONE_ID_UTC,
             Instant.now(),
-            false);
+            isBreak);
 
         when(timeEntryRepository.findById(42L)).thenReturn(Optional.of(existingEntity));
         when(timeEntryRepository.save(any(TimeEntryEntity.class))).thenAnswer(returnsFirstArg());
@@ -355,8 +361,9 @@ class TimeEntryServiceImplTest {
         assertThat(actualPersisted.isBreak()).isFalse();
     }
 
-    @Test
-    void ensureUpdateTimeEntryEndAndDuration() throws Exception {
+    @ParameterizedTest
+    @ValueSource(booleans = {true,false})
+    void ensureUpdateTimeEntryEndAndDuration(boolean isBreak) throws Exception {
 
         final Clock fixedClock = Clock.fixed(Instant.now(), UTC);
         sut = new TimeEntryServiceImpl(timeEntryRepository, userManagementService, userDateService, fixedClock);
@@ -375,7 +382,7 @@ class TimeEntryServiceImplTest {
             entryEnd.toInstant(UTC),
             ZONE_ID_UTC,
             Instant.now(),
-            false);
+            isBreak);
 
         when(timeEntryRepository.findById(42L)).thenReturn(Optional.of(existingEntity));
         when(timeEntryRepository.save(any(TimeEntryEntity.class))).thenAnswer(returnsFirstArg());
@@ -410,8 +417,9 @@ class TimeEntryServiceImplTest {
         assertThat(actualPersisted.isBreak()).isFalse();
     }
 
-    @Test
-    void ensureUpdateTimeEntryDuration() throws Exception {
+    @ParameterizedTest
+    @ValueSource(booleans = {true,false})
+    void ensureUpdateTimeEntryDuration(boolean isBreak) throws Exception {
 
         final Clock fixedClock = Clock.fixed(Instant.now(), UTC);
         sut = new TimeEntryServiceImpl(timeEntryRepository, userManagementService, userDateService, fixedClock);
@@ -430,7 +438,7 @@ class TimeEntryServiceImplTest {
             entryEnd.toInstant(UTC),
             ZONE_ID_UTC,
             Instant.now(),
-            false);
+            isBreak);
 
         when(timeEntryRepository.findById(42L)).thenReturn(Optional.of(existingEntity));
         when(timeEntryRepository.save(any(TimeEntryEntity.class))).thenAnswer(returnsFirstArg());