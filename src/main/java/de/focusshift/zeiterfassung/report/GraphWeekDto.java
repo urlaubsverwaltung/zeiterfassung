223fb89d2ad6eb5325668aaa692b9100bcff9e61
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-02-19 19:22:42 +0100

    add overtime to report month view

--- src/main/java/de/focusshift/zeiterfassung/report/GraphWeekDto.java
+++ src/main/java/de/focusshift/zeiterfassung/report/GraphWeekDto.java
@@ -6,7 +6,12 @@ record GraphWeekDto(String yearMonthWeek, List<GraphDayDto> dayReports, Double m
                     Double averageHoursWorked) {
 
     public Double graphLegendMaxHour() {
-        final Double maxShould = dayReports.stream().map(GraphDayDto::hoursWorkedShould).max(Double::compareTo).orElse(8.0);
+
+        final Double maxShould = dayReports.stream()
+            .map(GraphDayDto::hoursWorkedShould)
+            .max(Double::compareTo)
+            .orElse(8.0);
+
         final double max = Math.max(maxHoursWorked(), maxShould);
         return max % 2 == 0 ? max + 2 : max + 1;
     }
