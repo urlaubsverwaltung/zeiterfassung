6aa6c4285f3e05808c324553bb546cba1735068d
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2023-04-26 11:42:17 +0200

    Add help to avatar menu

---
+++ src/test/java/de/focusshift/zeiterfassung/web/MenuPropertiesTest.java
@@ -0,0 +1,54 @@
+package de.focusshift.zeiterfassung.web;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MenuPropertiesTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        try (final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+            validator = validatorFactory.getValidator();
+        }
+    }
+
+    @Test
+    void helpUrlDefault() {
+        final MenuProperties menuProperties = new MenuProperties();
+        assertThat(menuProperties.getHelp().getUrl()).isEqualTo("https://urlaubsverwaltung.cloud/hilfe/?source=open-source#dokumentation");
+    }
+
+    @Test
+    void helpUrlIsGiven() {
+        final MenuProperties menuProperties = new MenuProperties();
+        menuProperties.getHelp().setUrl("https://urlaubsverwaltung.cloud/hilfe/?source=open-source#dokumentation");
+        final Set<ConstraintViolation<MenuProperties>> violations = validator.validate(menuProperties);
+
+        assertThat(violations).isEmpty();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "NotAnEmail"})
+    @NullSource
+    void helpUrlIsWrong(String input) {
+        final MenuProperties menuProperties = new MenuProperties();
+        menuProperties.getHelp().setUrl(input);
+        final Set<ConstraintViolation<MenuProperties>> violations = validator.validate(menuProperties);
+
+        assertThat(violations.size()).isOne();
+    }
+
+}
