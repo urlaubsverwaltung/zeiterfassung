62da91a3c94b55436a7cfbe99fe1e99ff9a1376c
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-06-30 15:09:48 +0200

    use separate css files for components

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: [
+    require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
     isProduction && cssnano({ preset: "default" }),
