35a54b6ecf05467c42a34a93cb263e7b336c4c2d
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-07-05 23:41:07 +0200

    fix TimeEntryDay plannedWorkingHours usage
    - avoid calculating value on every get call
    - overtime has to use plannedWorkingHours, too

--- src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryDay.java
+++ src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryDay.java
@@ -10,22 +10,20 @@ import java.util.List;
 
 import static java.math.RoundingMode.CEILING;
 
+/**
+ *
+ * @param date of the time entry day
+ * @param plannedWorkingHours planned working hours including absences info
+ * @param timeEntries list of time entries
+ * @param absences list of absences. could be one FULL absence or two absences MORNING and NOON
+ */
 record TimeEntryDay(LocalDate date, PlannedWorkingHours plannedWorkingHours, List<TimeEntry> timeEntries, List<Absence> absences) {
 
-    public PlannedWorkingHours plannedWorkingHours() {
-
-        final double absenceDayLengthValue = absences.stream()
-            .map(Absence::dayLength)
-            .map(DayLength::getValue)
-            .reduce(0.0, Double::sum);
-
-        if (absenceDayLengthValue >= 1.0) {
-            return PlannedWorkingHours.ZERO;
-        } else if (absenceDayLengthValue == 0.5) {
-            return new PlannedWorkingHours(plannedWorkingHours.value().dividedBy(2));
-        }
-
-        return plannedWorkingHours;
+    TimeEntryDay(LocalDate date, PlannedWorkingHours plannedWorkingHours, List<TimeEntry> timeEntries, List<Absence> absences) {
+        this.date = date;
+        this.plannedWorkingHours = calculatePlannedWorkingHoursWithAbsences(plannedWorkingHours, absences);
+        this.timeEntries = timeEntries;
+        this.absences = absences;
     }
 
     /**
@@ -68,5 +66,21 @@ record TimeEntryDay(LocalDate date, PlannedWorkingHours plannedWorkingHours, Lis
         final BigDecimal ratio = BigDecimal.valueOf(worked).divide(BigDecimal.valueOf(planned), 2, CEILING);
 
         return ratio.compareTo(BigDecimal.ONE) > 0 ? BigDecimal.ONE : ratio;
+    }
+
+    private static PlannedWorkingHours calculatePlannedWorkingHoursWithAbsences(PlannedWorkingHours actuallyPlanned, List<Absence> absences) {
+
+        final double absenceDayLengthValue = absences.stream()
+            .map(Absence::dayLength)
+            .map(DayLength::getValue)
+            .reduce(0.0, Double::sum);
+
+        if (absenceDayLengthValue >= 1.0) {
+            return PlannedWorkingHours.ZERO;
+        } else if (absenceDayLengthValue == 0.5) {
+            return new PlannedWorkingHours(actuallyPlanned.value().dividedBy(2));
+        }
+
+        return actuallyPlanned;
     }
 }
