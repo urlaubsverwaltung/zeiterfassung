3ac4ec997fb0e4fe086357ee1098988e5227c45c
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-08-04 10:46:29 +0200

    add javadoc to prepared AbsenceTypes

--- src/main/java/de/focusshift/zeiterfassung/absence/AbsenceType.java
+++ src/main/java/de/focusshift/zeiterfassung/absence/AbsenceType.java
@@ -2,8 +2,23 @@ package de.focusshift.zeiterfassung.absence;
 
 public record AbsenceType(AbsenceTypeCategory category, Long sourceId) {
 
+    /**
+     * implicitly known HOLIDAY absence to ease testing.
+     * note: do not use in production code since sourceId is not ensured to be correct in runtime.
+     */
     public static AbsenceType HOLIDAY = new AbsenceType(AbsenceTypeCategory.HOLIDAY, 1000L);
+
+    /**
+     * implicitly known SPECIALLEAVE absence to ease testing.
+     * note: do not use in production code since sourceId is not ensured to be correct in runtime.
+     */
     public static AbsenceType SPECIALLEAVE = new AbsenceType(AbsenceTypeCategory.SPECIALLEAVE, 2000L);
+
+    /**
+     * implicitly known UNPAIDLEAVE absence to ease testing.
+     * note: do not use in production code since sourceId is not ensured to be correct in runtime.
+     */
     public static AbsenceType UNPAIDLEAVE = new AbsenceType(AbsenceTypeCategory.UNPAIDLEAVE, 3000L);
+
     public static AbsenceType SICK = new AbsenceType(AbsenceTypeCategory.SICK, null);
 }
