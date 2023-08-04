f90993cea9486da53ff63600c9df1cedd1fb3a17
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-08-04 10:59:01 +0200

    improve AbsenceTest

--- src/test/java/de/focusshift/zeiterfassung/absence/AbsenceTest.java
+++ src/test/java/de/focusshift/zeiterfassung/absence/AbsenceTest.java
@@ -1,22 +1,45 @@
 package de.focusshift.zeiterfassung.absence;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static de.focusshift.zeiterfassung.absence.AbsenceType.HOLIDAY;
-import static de.focusshift.zeiterfassung.absence.AbsenceType.SICK;
+import static de.focusshift.zeiterfassung.absence.AbsenceTypeCategory.HOLIDAY;
+import static de.focusshift.zeiterfassung.absence.AbsenceTypeCategory.SICK;
 import static de.focusshift.zeiterfassung.absence.DayLength.FULL;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AbsenceTest {
 
+    @ParameterizedTest
+    @EnumSource(value = AbsenceTypeCategory.class)
+    void ensureGetMessageKeyWithCategory(AbsenceTypeCategory givenCategory) {
+        final AbsenceType absenceType = new AbsenceType(givenCategory, 100L);
+        final Absence sut = new Absence(null, null, null, FULL, absenceType, null);
+        assertThat(sut.getMessageKey()).isEqualTo("absence.%s.100.FULL".formatted(givenCategory.name()));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = DayLength.class)
+    void ensureGetMessageKeyWithDayLength(DayLength givenDayLength) {
+        final AbsenceType absenceType = new AbsenceType(HOLIDAY, 100L);
+        final Absence sut = new Absence(null, null, null, givenDayLength, absenceType, null);
+        assertThat(sut.getMessageKey()).isEqualTo("absence.HOLIDAY.100.%s".formatted(givenDayLength.name()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {100L, 200L})
+    void ensureGetMessageKeyWithSourceId(Long givenSourceId) {
+        final AbsenceType absenceType = new AbsenceType(HOLIDAY, givenSourceId);
+        final Absence sut = new Absence(null, null, null, FULL, absenceType, null);
+        assertThat(sut.getMessageKey()).isEqualTo("absence.HOLIDAY.%s.FULL".formatted(givenSourceId));
+    }
+
     @Test
-    void getMessageKey() {
-
-        final Absence sick = new Absence(null, null, null, FULL, SICK, null);
-        final Absence holiday = new Absence(null, null, null, FULL, HOLIDAY, null);
-
-        assertThat(sick.getMessageKey()).isEqualTo("absence.SICK.FULL");
-        assertThat(holiday.getMessageKey()).isEqualTo("absence.HOLIDAY.1000.FULL");
+    void ensureGetMessageKeyWithSourceIdNull() {
+        final AbsenceType absenceType = new AbsenceType(SICK, null);
+        final Absence sut = new Absence(null, null, null, FULL, absenceType, null);
+        assertThat(sut.getMessageKey()).isEqualTo("absence.HOLIDAY.FULL");
     }
 }
