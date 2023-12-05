cfc287f5a4094a7a50984d217f599bbde7059c56
Benjamin Seber <benjamin.seber@focus-shift.de>
2023-12-05 20:08:24 +0100

    add archUnit test for hotwired/turbo controller methods

---
+++ src/main/java/de/focusshift/zeiterfassung/web/HotwiredTurboConstants.java
@@ -0,0 +1,12 @@
+package de.focusshift.zeiterfassung.web;
+
+public final class HotwiredTurboConstants {
+
+    public static final String TURBO_STREAM_MEDIA_TYPE = "text/vnd.turbo-stream.html";
+
+    public static final String TURBO_FRAME_HEADER = "Turbo-Frame";
+
+    private HotwiredTurboConstants() {
+        //
+    }
+}
