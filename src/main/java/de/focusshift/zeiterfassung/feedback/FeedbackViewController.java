65b6eb42e0121f317921f36b219e86428996e7b0
honnel <github@honnel.de>
2024-04-05 12:10:28 +0200

    Add missing @Controller annotation
    fixes #644

--- src/main/java/de/focusshift/zeiterfassung/feedback/FeedbackViewController.java
+++ src/main/java/de/focusshift/zeiterfassung/feedback/FeedbackViewController.java
@@ -4,10 +4,12 @@ import de.focusshift.zeiterfassung.tenancy.user.EMailAddress;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Controller
 @RequestMapping("/feedback")
 class FeedbackViewController {
 
