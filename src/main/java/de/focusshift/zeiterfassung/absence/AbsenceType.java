9197f12246d48a3efba70e0eb3d90e3ae90b1d76
Benjamin Seber <benjamin.seber@focus-shift.de>
2024-04-26 13:20:18 +0200

    add javadoc to AbsenceType

--- src/main/java/de/focusshift/zeiterfassung/absence/AbsenceType.java
+++ src/main/java/de/focusshift/zeiterfassung/absence/AbsenceType.java
@@ -5,6 +5,18 @@ import org.springframework.lang.Nullable;
 import java.util.Locale;
 import java.util.Map;
 
+/**
+ * Describes the absence-type of an {@linkplain Absence}. Absence types are not managed by Zeiterfassung, but by an
+ * external system.
+ *
+ * <p>
+ * Therefore, existing labels are unknown. There should be at least one, actually, but you have to handle {@code null}
+ * values yourself. Or {@linkplain Locale locales} that are not existing in {@linkplain #labelByLocale}.
+ *
+ * @param category {@linkplain AbsenceTypeCategory} of this {@linkplain AbsenceType}
+ * @param sourceId external id of this {@linkplain AbsenceType}
+ * @param labelByLocale label of this {@linkplain AbsenceType} for a given {@linkplain Locale}
+ */
 public record AbsenceType(
     AbsenceTypeCategory category,
     @Nullable Long sourceId,
