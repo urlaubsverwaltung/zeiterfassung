78cdffe4f0b36a67ae165ab96f12999c40ed4df6
Benjamin Seber <benjamin.seber@focus-shift.de>
2024-05-10 13:31:15 +0200

    add absence integration test

--- src/main/java/de/focusshift/zeiterfassung/absence/AbsenceTypeRepository.java
+++ src/main/java/de/focusshift/zeiterfassung/absence/AbsenceTypeRepository.java
@@ -1,12 +1,12 @@
 package de.focusshift.zeiterfassung.absence;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-interface AbsenceTypeRepository extends JpaRepository<AbsenceTypeEntity, Long> {
+interface AbsenceTypeRepository extends CrudRepository<AbsenceTypeEntity, Long> {
 
     Optional<AbsenceTypeEntity> findByTenantIdAndSourceId(String tenantId, Long sourceId);
 
