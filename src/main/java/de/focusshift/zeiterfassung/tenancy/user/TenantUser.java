8e9279e3fae617e12d84df5a9d69d5d5889b1832
Benjamin Seber <benjamin.seber@focus-shift.de>
2024-06-21 16:31:56 +0200

    remove unused constructor

--- src/main/java/de/focusshift/zeiterfassung/tenancy/user/TenantUser.java
+++ src/main/java/de/focusshift/zeiterfassung/tenancy/user/TenantUser.java
@@ -7,10 +7,6 @@ import java.util.Set;
 
 public record TenantUser(String id, Long localId, String givenName, String familyName, EMailAddress eMail, Instant firstLoginAt, Set<SecurityRole> authorities, Instant createdAt, Instant updatedAt, Instant deactivatedAt, Instant deletedAt, UserStatus status) {
 
-    public TenantUser(String id, Long localId, String givenName, String familyName, EMailAddress eMail, Set<SecurityRole> authorities) {
-        this(id, localId, givenName, familyName, eMail, null, authorities, null, null, null, null, UserStatus.UNKNOWN);
-    }
-
     @Override
     public String toString() {
         return "TenantUser{" +
