507585679b0156fbd54997fea282ada2c85f2803
Benjamin Seber <benjamin.seber@focus-shift.de>
2024-06-26 21:45:42 +0200

    remove properties validation annotations

--- src/main/java/de/focusshift/zeiterfassung/integration/portal/user/PortalUserRabbitmqConfigurationProperties.java
+++ src/main/java/de/focusshift/zeiterfassung/integration/portal/user/PortalUserRabbitmqConfigurationProperties.java
@@ -1,10 +1,7 @@
 package de.focusshift.zeiterfassung.integration.portal.user;
 
-import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
 
-@Validated
 @ConfigurationProperties("zeiterfassung.integration.portal.user")
 class PortalUserRabbitmqConfigurationProperties {
 
@@ -12,10 +9,8 @@ class PortalUserRabbitmqConfigurationProperties {
 
     private boolean manageTopology = false;
 
-    @NotEmpty
     private String topic = "portal.topic";
 
-    @NotEmpty
     private String routingKeyCreated = "created";
     private String routingKeyUpdated = "updated";
     private String routingKeyDeleted = "deleted";
