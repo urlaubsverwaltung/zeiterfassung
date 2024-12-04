d983e8f631b5efa403bfbb4eaa79f1b9917e8058
Tobias Schneider <Tobias.Schneider@Tobsch.org>
2024-12-04 22:14:08 +0100

    public holidays: add parts of the us

--- src/main/java/de/focusshift/zeiterfassung/publicholiday/PublicHolidayConfiguration.java
+++ src/main/java/de/focusshift/zeiterfassung/publicholiday/PublicHolidayConfiguration.java
@@ -4,22 +4,23 @@ import de.focus_shift.jollyday.core.HolidayManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 import static de.focus_shift.jollyday.core.HolidayManager.getInstance;
 import static de.focus_shift.jollyday.core.ManagerParameters.create;
+import static java.util.stream.Collectors.toMap;
 
 @Configuration
 class PublicHolidayConfiguration {
 
-    private static final List<String> COUNTRIES = List.of("de", "at", "ch", "gb", "gr", "mt", "it", "hr", "es", "nl", "lt", "be", "pl");
+    private static final List<String> COUNTRIES = List.of("de", "at", "ch", "gb", "gr", "mt", "it", "hr", "es", "nl", "lt", "be", "pl", "us");
 
     @Bean
     Map<String, HolidayManager> holidayManagerMap() {
-        final Map<String, HolidayManager> countryMap = new HashMap<>();
-        COUNTRIES.forEach(country -> countryMap.put(country, getInstance(create(country))));
-        return countryMap;
+        return COUNTRIES.stream()
+            .map(country -> getInstance(create(country)))
+            .collect(toMap(holidayManager -> holidayManager.getManagerParameter().getDisplayName(), Function.identity()));
     }
 }
---
+++ src/test/java/de/focusshift/zeiterfassung/publicholiday/PublicHolidayConfigurationTest.java
@@ -0,0 +1,19 @@
+package de.focusshift.zeiterfassung.publicholiday;
+
+import de.focus_shift.jollyday.core.HolidayManager;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PublicHolidayConfigurationTest {
+
+    @Test
+    void ensureThatHolidaysManagerMapContainsAllKeys() {
+        final Map<String, HolidayManager> holidayManagers = new PublicHolidayConfiguration().holidayManagerMap();
+        assertThat(holidayManagers)
+            .hasSize(14)
+            .containsKeys("de", "at", "ch", "gb", "gr", "mt", "it", "hr", "es", "nl", "lt", "be", "pl", "us");
+    }
+}
