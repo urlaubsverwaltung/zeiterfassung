041c2a54295b7bbce53bb24cc8fcf1b0aefecd3b
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-01-17 10:38:35 +0100

    use easier time-entry-history-list styling. and use responsive layout
    note the removed css hint that css subgrid does not work with the details element.
    so if we want to align the user in the summary element we have to replace the details element...

--- src/main/javascript/bundles/reports.ts
+++ src/main/javascript/bundles/reports.ts
@@ -2,19 +2,3 @@ import "../components/checkbox-all-option";
 import "../components/report-graph";
 import "../components/report-breakdown-section";
 import "../components/report-user-select";
-
-// css subgrid does not work on <details> elements
-// therefore we're using a div and doing this ourselves :/ without handling accessibility stuff currently...
-// TODO using click results in opening the thing despite user has just selected text for instance. we should use mouseup instead.
-document.body.addEventListener("click", (event) => {
-  const target = event.target as HTMLElement;
-  const summary = target.closest(".time-entry-history-list__element__summary");
-  if (summary) {
-    const parent = summary.parentElement;
-    if (parent.hasAttribute("open")) {
-      parent.removeAttribute("open");
-    } else {
-      parent.setAttribute("open", "");
-    }
-  }
-});
