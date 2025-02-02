53a0ec18a495430229dcdd1fd6629a3fa3363562
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-02-02 17:12:31 +0100

    add modal javascript tests

--- src/main/javascript/bundles/user-common.ts
+++ src/main/javascript/bundles/user-common.ts
@@ -1,7 +1,7 @@
 import "../components/avatar";
 import "../components/details-dropdown";
 import "../components/feedback-form";
-import "../components/modal";
+import { enhanceModal } from "../components/modal";
 import "../components/navigation";
 import "../components/time-clock";
 import { initPreventDoubleClickSubmit } from "../components/form";
@@ -10,6 +10,8 @@ import { initFeedbackHeartView } from "../components/feedback-heart";
 const showFeedbackKudo =
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (globalThis as any).zeiterfassung?.feedback?.showFeedbackKudo ?? false;
+
+enhanceModal();
 
 initFeedbackHeartView({
   target: document.body,
