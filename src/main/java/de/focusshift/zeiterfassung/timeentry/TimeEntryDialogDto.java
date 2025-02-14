7742dcbcacfc5ed29ed7a03fc240a837d955bd7f
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-02-14 15:30:04 +0100

    render edit form when user has authority to edit

--- src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryDialogDto.java
+++ src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryDialogDto.java
@@ -3,6 +3,7 @@ package de.focusshift.zeiterfassung.timeentry;
 import java.util.List;
 
 public record TimeEntryDialogDto(
+    boolean allowedToEdit,
     String owner,
     List<TimeEntryHistoryItemDto> historyItems,
     String editTimeEntryFormAction,
