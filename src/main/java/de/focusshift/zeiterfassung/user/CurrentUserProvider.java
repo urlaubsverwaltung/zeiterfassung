e4ffcda1c899356b483857c8ac13a40b426e6883
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-02-14 15:10:37 +0100

    introduce AuthenticationService

--- src/main/java/de/focusshift/zeiterfassung/user/CurrentUserProvider.java
+++ src/main/java/de/focusshift/zeiterfassung/user/CurrentUserProvider.java
@@ -1,11 +1,8 @@
 package de.focusshift.zeiterfassung.user;
 
 import de.focusshift.zeiterfassung.usermanagement.User;
-import org.springframework.security.core.Authentication;
 
 public interface CurrentUserProvider {
-
-    Authentication getCurrentAuthentication();
 
     User getCurrentUser();
 }
