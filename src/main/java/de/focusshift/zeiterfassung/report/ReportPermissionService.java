9dd617ee700ee90b7b0d8b9820dae0878080d861
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-02-14 15:10:37 +0100

    introduce AuthenticationService

--- src/main/java/de/focusshift/zeiterfassung/report/ReportPermissionService.java
+++ src/main/java/de/focusshift/zeiterfassung/report/ReportPermissionService.java
@@ -1,41 +1,32 @@
 package de.focusshift.zeiterfassung.report;
 
+import de.focusshift.zeiterfassung.security.AuthenticationService;
 import de.focusshift.zeiterfassung.user.CurrentUserProvider;
 import de.focusshift.zeiterfassung.usermanagement.User;
 import de.focusshift.zeiterfassung.usermanagement.UserLocalId;
 import de.focusshift.zeiterfassung.usermanagement.UserManagementService;
-import org.slf4j.Logger;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 import static de.focusshift.zeiterfassung.security.SecurityRole.ZEITERFASSUNG_VIEW_REPORT_ALL;
-import static java.lang.invoke.MethodHandles.lookup;
-import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
 class ReportPermissionService {
 
-    private static final Logger LOG = getLogger(lookup().lookupClass());
-
+    private final AuthenticationService authenticationService;
     private final CurrentUserProvider currentUserProvider;
     private final UserManagementService userManagementService;
 
-    ReportPermissionService(CurrentUserProvider currentUserProvider, UserManagementService userManagementService) {
+    ReportPermissionService(AuthenticationService authenticationService, CurrentUserProvider currentUserProvider,
+                            UserManagementService userManagementService) {
+        this.authenticationService = authenticationService;
         this.currentUserProvider = currentUserProvider;
         this.userManagementService = userManagementService;
     }
 
     boolean currentUserHasPermissionForAllUsers() {
-        final Authentication authentication = currentUserProvider.getCurrentAuthentication();
-        final boolean contains = authentication.getAuthorities().contains(ZEITERFASSUNG_VIEW_REPORT_ALL.authority());
-
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("user={} has permission={} for all users", authentication.getName(), contains);
-        }
-
-        return contains;
+        return authenticationService.hasSecurityRole(ZEITERFASSUNG_VIEW_REPORT_ALL);
     }
 
     List<UserLocalId> filterUserLocalIdsByCurrentUserHasPermissionFor(List<UserLocalId> userLocalIds) {
