fc61becd04e5e28cbe5c8f04d6f3bc430d7d6292
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-03-18 11:22:25 +0100

    refactor to reuse method

--- src/main/java/de/focusshift/zeiterfassung/security/AuthenticationFacade.java
+++ src/main/java/de/focusshift/zeiterfassung/security/AuthenticationFacade.java
@@ -34,8 +34,30 @@ public class AuthenticationFacade {
      * @return the {@link UserIdComposite} of the current user
      */
     public UserIdComposite getCurrentUserIdComposite() {
+        final Authentication authentication = getCurrentAuthentication();
+        return userIdCompositeFromAuth(authentication);
+    }
+
+    /**
+     * Checks whether the current {@link Authentication} has the given {@link SecurityRole} or not.
+     *
+     * @param securityRole {@link SecurityRole} to check
+     * @return <code>true</code> when the current authentication has the securityRole, <code>false</code> otherwise.
+     */
+    public boolean hasSecurityRole(SecurityRole securityRole) {
 
         final Authentication authentication = getCurrentAuthentication();
+        final boolean hasRole = authentication.getAuthorities().contains(securityRole.authority());
+
+        if (LOG.isDebugEnabled()) {
+            final UserIdComposite userIdComposite = userIdCompositeFromAuth(authentication);
+            LOG.debug("user={} has permission={}", userIdComposite.id(), hasRole);
+        }
+
+        return hasRole;
+    }
+
+    private static UserIdComposite userIdCompositeFromAuth(Authentication authentication) {
 
         if (authentication instanceof OAuth2AuthenticationToken token) {
             final OAuth2User oAuth2User = token.getPrincipal();
@@ -51,23 +73,5 @@ public class AuthenticationFacade {
         }
 
         throw new IllegalStateException("unexpected authentication token: " + authentication.getPrincipal());
-    }
-
-    /**
-     * Checks whether the current {@link Authentication} has the given {@link SecurityRole} or not.
-     *
-     * @param securityRole {@link SecurityRole} to check
-     * @return <code>true</code> when the current authentication has the securityRole, <code>false</code> otherwise.
-     */
-    public boolean hasSecurityRole(SecurityRole securityRole) {
-
-        final Authentication authentication = getCurrentAuthentication();
-        final boolean hasRole = authentication.getAuthorities().contains(securityRole.authority());
-
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("user={} has permission={}", authentication.getName(), hasRole);
-        }
-
-        return hasRole;
     }
 }
