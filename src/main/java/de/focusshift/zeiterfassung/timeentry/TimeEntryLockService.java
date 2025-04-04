0f00f3d7fbcd01fbb2f665615ea6394e5f9baaf4
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-04-04 22:04:02 +0200

    WIP: prevent TimeEntry creation/update then timespan is locked
    TODO:
    * frontend error messages / feedback
    * tests

---
+++ src/main/java/de/focusshift/zeiterfassung/timeentry/TimeEntryLockService.java
@@ -0,0 +1,27 @@
+package de.focusshift.zeiterfassung.timeentry;
+
+import de.focusshift.zeiterfassung.security.SecurityRole;
+
+import java.time.temporal.Temporal;
+import java.util.Collection;
+
+public interface TimeEntryLockService {
+
+    /**
+     * Checks whether the given timespan is locked or not. This can be configured by privileged persons with
+     * {@link de.focusshift.zeiterfassung.settings.LockTimeEntriesSettings}
+     *
+     * @param start start date of the timespan
+     * @param end end date of the timespan
+     * @return {@code true} when the timespan is locked, {@code false} otherwise
+     */
+    boolean isTimespanLocked(Temporal start, Temporal end);
+
+    /**
+     * Checks whether the given user is allowed to create, edit or delete {@link TimeEntry} despite a locked timespan.
+     *
+     * @param roles user roles to check
+     * @return {@code true} when the user is allowed to bypass the lock, {@link false} otherwise
+     */
+    boolean isUserAllowedToBypassLock(Collection<SecurityRole> roles);
+}
