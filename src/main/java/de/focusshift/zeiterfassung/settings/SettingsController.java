7a4b8d380247534ec45fbfb79ac91d738a57a772
honnel <github@honnel.de>
2025-04-04 15:37:24 +0200

    Add lock time entry settings
    Add settings for locking timeentries:
    * enable/disable locking of timeentries
    * days in past time entries get locked
    
    Introduces global permission for configure settings
    ZEITERFASSUNG_SETTINGS_GLOBAL which will replace
    ZEITERFASSUNG_WORKING_TIME_EDIT_GLOBAL.
    
    Why input type=string inputmode=numeric instead of input type=number?
    --> https://technology.blog.gov.uk/2020/02/24/why-the-gov-uk-design-system-team-changed-the-input-type-for-numbers/

--- src/main/java/de/focusshift/zeiterfassung/settings/SettingsController.java
+++ src/main/java/de/focusshift/zeiterfassung/settings/SettingsController.java
@@ -2,9 +2,11 @@ package de.focusshift.zeiterfassung.settings;
 
 import de.focus_shift.launchpad.api.HasLaunchpad;
 import de.focusshift.zeiterfassung.timeclock.HasTimeClock;
+import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,11 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import static de.focusshift.zeiterfassung.settings.FederalStateSelectDtoFactory.federalStateSelectDto;
+import static java.util.Objects.requireNonNullElse;
 
 @Controller
 @RequestMapping("/settings")
-@PreAuthorize("hasAuthority('ZEITERFASSUNG_WORKING_TIME_EDIT_GLOBAL')")
+@PreAuthorize("hasAnyAuthority('ZEITERFASSUNG_WORKING_TIME_EDIT_GLOBAL', 'ZEITERFASSUNG_SETTINGS_GLOBAL')")
 class SettingsController implements HasLaunchpad, HasTimeClock {
+
+    private static final String ATTRIBUTE_NAME_SETTINGS = "settings";
 
     private final SettingsService settingsService;
 
@@ -26,35 +31,60 @@ class SettingsController implements HasLaunchpad, HasTimeClock {
     }
 
     @GetMapping
-    String getSettings() {
-        return "redirect:settings/federal-state";
-    }
-
-    @GetMapping("/federal-state")
-    String getFederalStateSettings(Model model) {
-
-        final FederalStateSettings settings = settingsService.getFederalStateSettings();
-        final FederalStateSettingsDto federalStateSettingsDto = toFederalStateSettingsDto(settings);
-
-        model.addAttribute("federalStateSettings", federalStateSettingsDto);
-        model.addAttribute("federalStateSelect", federalStateSelectDto(federalStateSettingsDto.federalState()));
-
+    String getSettings(Model model) {
+        fillFederalStateSettings(model);
         return "settings/settings";
     }
 
-    @PostMapping("/federal-state")
-    ModelAndView saveSettings(@ModelAttribute("federalStateSettings") FederalStateSettingsDto federalStateSettings, BindingResult result) {
+    void fillFederalStateSettings(Model model) {
 
-        if (result.hasErrors()) {
+        final FederalStateSettings federalStateSettings = settingsService.getFederalStateSettings();
+        final LockTimeEntriesSettings lockTimeEntriesSettings = settingsService.getLockTimeEntriesSettings();
+        final SettingsDto settingsDto = toSettingsDto(federalStateSettings, lockTimeEntriesSettings);
+
+        prepareModel(model, settingsDto);
+    }
+
+    @PostMapping
+    ModelAndView saveSettings(@Valid @ModelAttribute(ATTRIBUTE_NAME_SETTINGS) SettingsDto settingsDto, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.getTarget() == null) {
+            // entering a string for lockTimeEntriesDaysInPast results in a TypeMismatchException which skips
+            // spring bean validation and therefore there is no binding yet.
+            bindingResult = new BeanPropertyBindingResult(settingsDto, ATTRIBUTE_NAME_SETTINGS);
+        }
+
+        final int lockTimeEntriesDaysInPast = requireNonNullElse(settingsDto.lockTimeEntriesDaysInPast(), -1);
+        if (settingsDto.lockingIsActive() && lockTimeEntriesDaysInPast < 0)  {
+            bindingResult.rejectValue( "lockTimeEntriesDaysInPast", "settings.lock-timeentries-days-in-past.validation.positiveOrZero");
+        }
+
+        if (bindingResult.hasErrors()) {
+            prepareModel(model, settingsDto);
+            model.addAttribute(BindingResult.MODEL_KEY_PREFIX + ATTRIBUTE_NAME_SETTINGS, bindingResult);
             return new ModelAndView("settings/settings");
         }
 
-        settingsService.updateFederalStateSettings(federalStateSettings.federalState(), federalStateSettings.worksOnPublicHoliday());
+        settingsService.updateFederalStateSettings(settingsDto.federalState(), settingsDto.worksOnPublicHoliday());
+        settingsService.updateLockTimeEntriesSettings(settingsDto.lockingIsActive(), lockTimeEntriesDaysInPast);
 
-        return new ModelAndView("redirect:/settings/federal-state");
+        return new ModelAndView("redirect:/settings");
     }
 
-    private FederalStateSettingsDto toFederalStateSettingsDto(FederalStateSettings federalStateSettings) {
-        return new FederalStateSettingsDto(federalStateSettings.federalState(), federalStateSettings.worksOnPublicHoliday());
+    private void prepareModel(Model model, SettingsDto settingsDto) {
+        model.addAttribute(ATTRIBUTE_NAME_SETTINGS, settingsDto);
+        model.addAttribute("federalStateSelect", federalStateSelectDto(settingsDto.federalState()));
+    }
+
+    private SettingsDto toSettingsDto(FederalStateSettings federalStateSettings, LockTimeEntriesSettings lockTimeEntriesSettings) {
+
+        final int lockTimeEntriesDaysInPast = lockTimeEntriesSettings.lockTimeEntriesDaysInPast();
+
+        return new SettingsDto(
+            federalStateSettings.federalState(),
+            federalStateSettings.worksOnPublicHoliday(),
+            lockTimeEntriesSettings.lockingIsActive(),
+            lockTimeEntriesDaysInPast >= 0 ? lockTimeEntriesDaysInPast : null
+        );
     }
 }
