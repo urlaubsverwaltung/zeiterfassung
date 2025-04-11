0be775ec9ea50dc96334148746799ec086ff8293
honnel <github@honnel.de>
2025-04-11 16:05:45 +0200

    Validate lock configuration for time clock edit

--- src/main/java/de/focusshift/zeiterfassung/timeclock/TimeClockController.java
+++ src/main/java/de/focusshift/zeiterfassung/timeclock/TimeClockController.java
@@ -3,9 +3,13 @@ package de.focusshift.zeiterfassung.timeclock;
 import de.focus_shift.launchpad.api.HasLaunchpad;
 import de.focusshift.zeiterfassung.security.CurrentUser;
 import de.focusshift.zeiterfassung.security.oidc.CurrentOidcUser;
+import de.focusshift.zeiterfassung.timeentry.TimeEntryLockService;
 import de.focusshift.zeiterfassung.user.UserId;
+import de.focusshift.zeiterfassung.user.UserSettingsProvider;
+import de.focusshift.zeiterfassung.usermanagement.UserLocalId;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,11 +22,16 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
 import static de.focusshift.zeiterfassung.web.HotwiredTurboConstants.TURBO_FRAME_HEADER;
 import static java.lang.String.format;
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.PRECONDITION_REQUIRED;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
@@ -32,12 +41,15 @@ import static org.springframework.util.StringUtils.hasText;
 @RequestMapping("timeclock")
 class TimeClockController implements HasTimeClock, HasLaunchpad {
 
+    private static final Logger LOG = getLogger(lookup().lookupClass());
     private static final String IS_REDIRECTED = "isRedirected";
 
     private final TimeClockService timeClockService;
+    private final TimeEntryLockService timeEntryLockService;
 
-    TimeClockController(TimeClockService timeClockService) {
+    TimeClockController(TimeClockService timeClockService, TimeEntryLockService timeEntryLockService) {
         this.timeClockService = timeClockService;
+        this.timeEntryLockService = timeEntryLockService;
     }
 
     @GetMapping
@@ -59,6 +71,13 @@ class TimeClockController implements HasTimeClock, HasLaunchpad {
         @RequestHeader(name = TURBO_FRAME_HEADER, required = false) String turboFrame,
         RedirectAttributes redirectAttributes
     ) {
+        timeEntryLockService.getMinValidTimeEntryDate().ifPresent(date -> {
+         if (timeClockUpdateDto.getDate().isBefore(date) || !timeEntryLockService.isUserAllowedToBypassLock(currentUser.getRoles())) {
+                LOG.info("Editing TimeClock is not allowed since currentUser is not privileged to bypass timespan lock.");
+                errors.reject("time-entry.validation.timespan.locked");
+            }
+        });
+
         if (errors.hasErrors()) {
             if (hasText(turboFrame)) {
                 return new ModelAndView("timeclock/timeclock-edit-form::navigation-box-update", UNPROCESSABLE_ENTITY);
