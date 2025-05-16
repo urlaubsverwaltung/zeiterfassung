7d2dbf6e888b4a4794af21853b2b291c9df787bd
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-05-16 14:17:22 +0200

    do not publish ZERO overtime to rabbit
    closes #1288

--- src/test/java/de/focusshift/zeiterfassung/overtime/OvertimePublisherIT.java
+++ src/test/java/de/focusshift/zeiterfassung/overtime/OvertimePublisherIT.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.event.ApplicationEvents;
 import org.springframework.test.context.event.RecordApplicationEvents;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
@@ -62,6 +63,33 @@ class OvertimePublisherIT extends SingleTenantTestContainersBase {
         assertThat(actualEvents).contains(
             new UserHasMadeOvertimeEvent(userId1, date, OvertimeHours.EIGHT_POSITIVE),
             new UserHasMadeOvertimeEvent(userId2, date, OvertimeHours.EIGHT_NEGATIVE)
+        );
+    }
+
+    @Test
+    void ensureUserHasMadeOvertimeIsNotPublishedWhenOvertimeIsZero() {
+
+        final LocalDate date = LocalDate.now();
+
+        final UserIdComposite userId = new UserIdComposite(new UserId("batman"), new UserLocalId(1L));
+        final UserIdComposite userId2= new UserIdComposite(new UserId("robin"), new UserLocalId(2L));
+
+        when(overtimeService.getOvertimeForDate(date)).thenReturn(Map.of(
+            userId, OvertimeHours.ZERO,
+            userId2, new OvertimeHours(Duration.ofSeconds(1))
+        ));
+
+        when(overtimeAccountService.getAllOvertimeAccounts()).thenReturn(Map.of(
+            userId, new OvertimeAccount(userId, true),
+            userId2, new OvertimeAccount(userId, true)
+        ));
+
+        final DayLockedEvent dayLockedEvent = new DayLockedEvent(date);
+        applicationEventPublisher.publishEvent(dayLockedEvent);
+
+        final List<UserHasMadeOvertimeEvent> actualEvents = applicationEvents.stream(UserHasMadeOvertimeEvent.class).toList();
+        assertThat(actualEvents).contains(
+            new UserHasMadeOvertimeEvent(userId2, date, new OvertimeHours(Duration.ofSeconds(1)))
         );
     }
 
