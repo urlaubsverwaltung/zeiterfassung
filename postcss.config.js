b5f3b7689c9bf8bc274f110cdfe1de00173aa842
Benjamin Seber <benjamin.seber@focus-shift.de>
2025-06-20 13:55:01 +0200

    bump tailwindcss from 3 to 4
    closes #4

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("autoprefixer"),
+    require("@tailwindcss/postcss"),
     isProduction && cssnano({ preset: "default" }),
   ].filter(Boolean),
 };
