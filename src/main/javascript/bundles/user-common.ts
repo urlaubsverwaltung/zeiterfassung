938e396992bda72ba7425a865227773e2aed2d7c
Benjamin Seber <benjamin.seber@focus-shift.de>
2026-03-14 21:38:47 +0100

    add user search to timeentries page, for authorized users
    closes #1293

--- src/main/javascript/bundles/user-common.ts
+++ src/main/javascript/bundles/user-common.ts
@@ -4,11 +4,15 @@ import { enhanceModal } from "../components/modal";
 import "../components/navigation";
 import "../components/time-clock";
 import "../components/tooltip";
-import { initPreventDoubleClickSubmit } from "../components/form";
+import {
+  initAutosubmit,
+  initPreventDoubleClickSubmit,
+} from "../components/form";
 import { FeedbackHeart } from "../components/feedback-heart";
 
 enhanceModal();
 initPreventDoubleClickSubmit();
+initAutosubmit();
 
 const feedbackHeart = new FeedbackHeart();
 feedbackHeart.classList.add("fixed", "left-0", "bottom-0", "p-2");
