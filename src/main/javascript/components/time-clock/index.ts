5407193bb999b02fdab92028d76acbed6cc56e83
Benjamin Seber <benjamin.seber@focus-shift.de>
2026-04-15 19:55:45 +0200

    fix closing time-clock popover on edit

--- src/main/javascript/components/time-clock/index.ts
+++ src/main/javascript/components/time-clock/index.ts
@@ -1,2 +1,1 @@
-import "./time-clock-edit";
 import "./TimeClockDuration";
--- src/main/javascript/components/time-clock/time-clock-edit.ts
+++
@@ -1,9 +0,0 @@
-import { onTurboFrameRender } from "../../turbo-events";
-
-onTurboFrameRender(function (event) {
-  const target = event.target as Element;
-  if (target.id === "frame-nav-time-clock-edit") {
-    const form = target.querySelector("form")!;
-    form.closest("details")!.open = form.dataset.hasErrors === "true";
-  }
-});
