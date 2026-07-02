af08cd752a5622b23cdd9006407749f66e156dfb
Benjamin Seber <benjamin.seber@focus-shift.de>
2026-07-02 09:50:16 +0200

    use fixed clock in TimeClockUIIT

--- src/main/java/de/focusshift/zeiterfassung/config/ClockConfig.java
+++ src/main/java/de/focusshift/zeiterfassung/config/ClockConfig.java
@@ -1,7 +1,9 @@
 package de.focusshift.zeiterfassung.config;
 
+import org.hibernate.validator.HibernateValidatorConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.time.Clock;
 
@@ -11,5 +13,16 @@ class ClockConfig {
     @Bean
     public Clock clock() {
         return Clock.systemUTC();
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean defaultValidator(Clock clock) {
+        final LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setConfigurationInitializer(configuration -> {
+            if (configuration instanceof HibernateValidatorConfiguration hibernateConfiguration) {
+                hibernateConfiguration.clockProvider(() -> clock);
+            }
+        });
+        return bean;
     }
 }
