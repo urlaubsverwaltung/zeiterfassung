6f4d02f55e146bd9a1986118480aa0fb79685b45
roboes <rg.roberto@outlook.com>
2026-08-17 08:45:45 +0200

    Fix failing tests

--- src/test/java/de/focusshift/zeiterfassung/report/ReportCsvServiceIT.java
+++ src/test/java/de/focusshift/zeiterfassung/report/ReportCsvServiceIT.java
@@ -1,20 +1,30 @@
 package de.focusshift.zeiterfassung.report;
 
 import de.focusshift.zeiterfassung.SingleTenantTestContainersBase;
+import de.focusshift.zeiterfassung.security.oidc.CurrentOidcUser;
 import de.focusshift.zeiterfassung.tenancy.user.EMailAddress;
 import de.focusshift.zeiterfassung.user.UserId;
 import de.focusshift.zeiterfassung.user.UserIdComposite;
 import de.focusshift.zeiterfassung.usermanagement.User;
 import de.focusshift.zeiterfassung.usermanagement.UserLocalId;
 import de.focusshift.zeiterfassung.usermanagement.UserManagementService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.io.PrintWriter;
+import java.time.Year;
 import java.time.YearMonth;
+import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.Set;
@@ -31,6 +41,11 @@ class ReportCsvServiceIT extends SingleTenantTestContainersBase {
 
     @MockitoBean
     private UserManagementService userManagementService;
+
+    @AfterEach
+    void clearSecurityContext() {
+        SecurityContextHolder.clearContext();
+    }
 
     @ParameterizedTest
     @CsvSource({"de,Datum;Vorname;Nachname;Von;Bis;Erfasste Stunden;Sollarbeitszeit;Kommentar;Pause", "en,Date;Given name;Family name;From;To;Worked hours;Should working hours;Comment;Break"})
@@ -50,7 +65,7 @@ class ReportCsvServiceIT extends SingleTenantTestContainersBase {
     }
 
     @ParameterizedTest
-    @CsvSource({"de,KW;Von;Bis;Vorname;Nachname;Erfasste Stunden;Sollarbeitszeit", "en,CW;From;To;Given name;Family name;Worked hours;Should working hours"})
+    @CsvSource({"de,Monat;Von;Bis;Vorname;Nachname;Erfasste Stunden;Sollarbeitszeit", "en,Month;From;To;Given name;Family name;Worked hours;Should working hours"})
     void ensureI18nHeaderAggregated(String languageTag, String expectedHeader) {
         final PrintWriter printWriter = mock(PrintWriter.class);
 
@@ -62,6 +77,31 @@ class ReportCsvServiceIT extends SingleTenantTestContainersBase {
         when(userManagementService.findUserById(userId)).thenReturn(Optional.of(user));
 
         sut.writeMonthReportCsvAggregated(YearMonth.of(2022, 9), Locale.forLanguageTag(languageTag), userId, printWriter);
+
+        verify(printWriter).println(expectedHeader);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"de,KW;Von;Bis;Vorname;Nachname;Erfasste Stunden;Sollarbeitszeit", "en,CW;From;To;Given name;Family name;Worked hours;Should working hours"})
+    void ensureI18nHeaderWeekAggregated(String languageTag, String expectedHeader) {
+        final PrintWriter printWriter = mock(PrintWriter.class);
+
+        final UserId userId = new UserId("user");
+        final UserLocalId userLocalId = new UserLocalId(1L);
+        final UserIdComposite userIdComposite = new UserIdComposite(userId, userLocalId);
+        final User user = new User(userIdComposite, "Bruce", "Wayne", new EMailAddress(""), Set.of());
+
+        when(userManagementService.findUserByLocalId(userLocalId)).thenReturn(Optional.of(user));
+        when(userManagementService.findAllUsersByLocalIds(List.of(userLocalId))).thenReturn(List.of(user));
+
+        // permission check for the week report requires an authenticated current user
+        final OidcIdToken idToken = OidcIdToken.withTokenValue("token-value").claim("sub", userId.value()).build();
+        final OidcUserInfo userInfo = OidcUserInfo.builder().subject(userId.value()).name("Bruce Wayne").build();
+        final CurrentOidcUser currentOidcUser = new CurrentOidcUser(new DefaultOidcUser(List.of(), idToken, userInfo), List.of(), List.of(), userLocalId);
+        final Authentication authentication = new OAuth2AuthenticationToken(currentOidcUser, List.of(), "registrationId");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        sut.writeWeekReportCsvAggregated(Year.of(2022), 36, Locale.forLanguageTag(languageTag), userLocalId, printWriter);
 
         verify(printWriter).println(expectedHeader);
     }
